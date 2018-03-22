65916e15c410d506ee1094eaf408449c42ab870f
Litrin J <litrin.jiang@intel.com>
2018-03-22 13:35:19 +0800

    ver0.6

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 
     name='ExperimentalDataReader',
-    version='0.5',
-    packages=find_packages("DataReader"),
+    version='0.6',
+    packages=find_packages(),
     url='https://github.intel.com/liqunjia/ExperimentalDataReader',
     license='BSD',
     author='Litrin Jiang',
