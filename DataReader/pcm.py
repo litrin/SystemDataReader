1d83c760187aae29954fd4560c668880ec1d5d3d
Litrin J <litrin.jiang@intel.com>
2019-01-07 07:36:27 +0800

    code refine

--- DataReader/pcm.py
+++ DataReader/pcm.py
@@ -3,27 +3,41 @@ import pandas as pd
 
 class BasePMCCSVReader:
     filename = None
-    sep = ";"
+    separator = ";"
 
-    def __init__(self, filename, default_sep=";"):
+    def __init__(self, filename, default_sep=None):
+        """
+        Read csv file from pcm tools.
+
+        :param filename: str
+        :param default_sep: str
+        """
         self.filename = filename
-        self.sep = default_sep
+        if default_sep is not None:
+            self.separator = default_sep
 
     def get_data_frame(self):
-
-        return pd.read_csv(self.filename, sep=self.sep, index_col = False,
+        return pd.read_csv(self.filename, sep=self.separator, index_col=False,
                            names=self.headers, skiprows=2)
 
     @property
     def headers(self):
+        """
+        PCM has 2 level column.
+        This function/parameter is helper to covert them to single column.
+
+        :return: list
+        """
         fd = open(self.filename, "r")
         file_head = fd.readlines(2)
-        zip_heads = zip(file_head[0].split(self.sep),
-                        file_head[1].split(self.sep))
+        fd.close()
 
+        zip_heads = zip(file_head[0].split(self.separator),
+                        file_head[1].split(self.separator))
         metric_names = []
         category = ""
         for _category, metric in zip_heads:
+            # fill empty category names
             if len(_category) is not 0:
                 category = _category
 
@@ -31,6 +45,20 @@ class BasePMCCSVReader:
 
         return metric_names[:-1]
 
+    @property
+    def data(self):
+        return self.get_data_frame()
+
 
 class PCMCSVReader(BasePMCCSVReader):
+    """
+    Ready for outputs from pcm.x
+    """
+    pass
+
+
+class PCMMemoryCSVReader(BasePMCCSVReader):
+    """
+    Ready for outputs from pcm-memory.x
+    """
     pass
