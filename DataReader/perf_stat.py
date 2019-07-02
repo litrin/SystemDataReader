7efcc0929576f7ee4d65cd9c53395a25cb8eda6e
Litrin J <litrin.jiang@intel.com>
2019-07-02 15:31:35 +0800

    new perf stat reader

--- DataReader/perf_stat.py
+++ DataReader/perf_stat.py
@@ -4,25 +4,38 @@ from .base import RawDataFileReader, DataCacheObject
 
 
 class PerfStatReader(RawDataFileReader, DataCacheObject):
+    """
+    perf stat out put file reader
+    example:
+
+    perf stat -e cycles,instructions  -I 100 -o event.out
+
+    todo: need to add cgroup info into dataframe
+    """
+    reg = r"\s+\d+\.\d+\s+(\d|\,)+"
 
     def __init__(self, filename):
         self.filename = filename
 
-    def get_event(self, event_name, regex=False):
-        if not regex:
-            event_name = ".*%s.*" % event_name.lower()
-
-        for row in list(self.grep_iterator(event_name)):
+    def get_content(self):
+        data = []
+        for row in self.grep_iterator(self.reg):
             row = row.split()
+            ts = float(row[0])
             value = int(row[1].replace(",", ""))
-            yield value
+            event = row[2]
 
-    def __getattr__(self, item):
-        return pd.Series(self.get_event(item))
+            data.append([ts, value, event])
+
+        df = pd.DataFrame(data, columns=["ts", "value", "event"])
+        return df
+
+    @property
+    def header(self):
+        return self.data["event"].unique()
 
     def __getitem__(self, item):
-        return self.get_content(list(item))
+        if item not in self.header:
+            raise IndexError("Can not find event: '%s' in this file" % item)
 
-    def get_content(self, columns):
-        data = {field: getattr(self, field) for field in columns}
-        return pd.DataFrame(data)
+        return self.data[self.data["event"] == item]
