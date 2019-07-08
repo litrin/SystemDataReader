c9ea1377f889919df8743741cca4bccf3c51441d
Litrin J <litrin.jiang@intel.com>
2019-07-08 14:15:15 +0800

    opt interface

--- DataReader/SPECcpu.py
+++ DataReader/SPECcpu.py
@@ -7,17 +7,15 @@ class SPECCPU2006Score(RawDataFileReader):
 
     score_row = []
 
-    def __init__(self, filename=r"./CINT2006.001.ref.txt", component=None):
+    def __init__(self, filename=r"CINT2006.001.ref.txt", component=None):
         self.filename = filename
+
         if component is not None:
-            self.default_component = component.lower()
+            self.set_component(component)
 
-    def set_component(self, component=None):
-        if component is None:
-            component = self.default_component
-        else:
-            component = component.lower()
-
+    def set_component(self, component):
+        component = component.lower()
+        # full component format
         regex = r"^\d{3}\.%s+(\s+\d+){3}" % component
 
         score_content = self.egrep(regex).pop()
@@ -38,3 +36,7 @@ class SPECCPU2006Score(RawDataFileReader):
     @property
     def rate(self):
         return int(self.score_row[3])
+
+    def __getitem__(self, item):
+        self.set_component(item)
+        return self.rate
