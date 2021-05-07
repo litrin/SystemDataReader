d9ec585cb946a197bee37e4e38bd8c9dee43c3e3
liqunjia <Tyui%678>
2021-05-07 12:14:52 +0800

    supports max trubo freq benchmarks

--- DataReader/ptumon.py
+++ DataReader/ptumon.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from DataReader.base import RawDataFileReader
@@ -75,3 +76,88 @@ class PtumonICX(BasePtuReader):
 
 Ptumon1 = PtumonSKX
 Ptumon2 = PtumonICX
+
+
+class PtuTurboFrequency(RawDataFileReader):
+    """
+    cli: ./ptu -ct 8 -avx [1|2|3] > filename.txt
+    """
+    __version__ = 2.0
+    HEADER = ['Instr', 'CPU', 'Cores', 'CFreq(act)', 'CFreq(exp)', 'UFreq',
+              'Util(act)', 'Util(exp)', 'Power', 'TDP', 'Temp', 'Volt',
+              'UVolt']
+    Instructions = None
+    data = None
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.read_file()
+
+    def read_file(self):
+        capture = False
+        data = []
+        for row in self.reader():
+            if len(row) < 2:
+                continue
+            if row.startswith("### TURBO is enabled ###"):
+                capture = True
+                continue
+
+            if capture:
+                row = row.split()
+                if len(row) < 13:
+                    continue
+                if "CPU" in row:
+                    continue
+                else:
+                    self.Instructions = row[0]
+                    for i, v in enumerate(row):
+                        if i == 0:
+                            continue
+                        if i == 1 or i == 2:
+                            row[i] = int(v)
+                            continue
+                        row[i] = float(v)
+
+                    data.append(dict(zip(self.HEADER, row)))
+
+        df = pd.DataFrame(data)
+        self.data = df
+
+    def get_by_cpu(self, cpu):
+        return self.data[self.data.CPU == cpu]
+
+    def get_by_cores(self, cores):
+        return self.data[self.data["Cores"] == cores]
+
+    def plot_all(self, filename=None):
+        core_list = self.data["CPU"].unique()
+        fig, axs = plt.subplots(len(core_list), 1)
+
+        for cpu in self.data["CPU"].unique():
+            data = self.get_by_cpu(cpu)
+            ax = axs[cpu]
+            ax.plot(data.Cores, data["CFreq(exp)"], color="red",
+                    linestyle="--", linewidth=0.75)
+            ax.plot(data.Cores, data["CFreq(act)"], linewidth=1)
+
+            ax.fill_between(data.Cores, data["CFreq(exp)"], data["CFreq(act)"],
+                            facecolor='r', alpha=0.3)
+            ax.grid(True)
+
+            ax.set_title("Socket %s (%s)" % (cpu, self.Instructions),
+                         fontsize=10)
+            ax.set_xlabel("Cores", fontsize=8)
+            ax.set_ylabel("MaxFreq(GHz)", fontsize=8)
+            ax.autoscale_view(True)
+
+        fig.tight_layout()
+
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(filename, dpi=600, transparent=True, pad_inches=0)
+        plt.close()
+
+    def __getitem__(self, item):
+        return self.get_by_cpu(item)
