5dff04f71d66e2c347fa971395235a3b98d59614
liqunjia <litrin.jiang@intel.com>
2022-01-04 09:30:56 +0800

    SPECcpu name translator

--- DataReader/SPECcpu.py
+++ DataReader/SPECcpu.py
@@ -1,3 +1,5 @@
+import re
+
 from DataReader.base import RawDataFileReader
 
 __all__ = ["SPECCPU2006Score", "SPECCPU2017Score"]
@@ -51,3 +53,67 @@ class SPECCPU2006Score(BaseSPECcpuScore):
 
 class SPECCPU2017Score(BaseSPECcpuScore):
     filename = "CPU2017.001.fpspeed.refspeed.txt"
+
+
+class SPECCPU2017Components:
+    dictionary = {
+        "500": "perlbench_r",
+        "502": "gcc_r",
+        "505": "mcf_r",
+        "520": "omnetpp_r",
+        "523": "xalancbmk_r",
+        "525": "x264_r",
+        "531": "deepsjeng_r",
+        "541": "leela_r",
+        "548": "exchange2_r",
+        "557": "xz_r",
+
+        "503": "bwaves_r",
+        "507": "cactuBSSN_r",
+        "519": "lbm_r",
+        "521": "wrf_r",
+        "527": "cam4_r",
+        "528": "pop2_r",  # only for test "speed"
+        "538": "imagick_r",
+        "544": "nab_r",
+        "549": "fotonik3d_r",
+        "554": "roms_r",
+
+    }
+
+    _reg = re.compile(r"^([5|6]\d{2})?\.?(\S*\_?[r|s]?)?$")
+
+    comp_id = 0
+    name = "unknown"
+    type = "rate"
+
+    def __init__(self, name):
+        group = self._reg.search(name).groups()
+
+        if group[0] is not None:
+            self.comp_id = int(group[0])
+        elif group[1] is not None:
+            self.name = group[1]
+        else:
+            raise NameError("%s is not a SPECcpu2017 component name")
+
+        if self.name[-2:] == "_s" or self.comp_id > 599:
+            self.type = "speed"
+            self.name = self.dictionary[str(self.comp_id - 100)]
+            self.name = "%ss" % (self.name[:-1])
+        else:
+            self.type = "rate"
+            if self.comp_id == 0:
+                for k, v in self.dictionary.items():
+                    if v == self.name:
+                        break
+                self.comp_id = k
+            if self.name == "unknown":
+                self.name = self.dictionary[str(self.comp_id)]
+
+    @property
+    def full_name(self):
+        return "%s.%s" % (self.comp_id, self.name)
+
+    def __str__(self):
+        return self.full_name
